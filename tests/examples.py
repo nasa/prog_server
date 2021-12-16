ac16a84c2920dbf25c2a0fdead43b985ee5be28d
Christopher Teubert <christopher.a.teubert@nasa.gov>
2021-12-16 15:52:37 -0800

    Test Examples

--- tests/examples.py
+++ tests/examples.py
@@ -2,9 +2,19 @@
 
 import unittest
 import time
-import prog_client, prog_server
+import prog_server
+import sys
+from io import StringIO 
+import pkgutil
+from importlib import import_module
 
 TIMEOUT = 10  # Server startup timeout in seconds
+
+def make_test_function(example):
+    def test(self):
+        ex = import_module("examples." + example)
+        ex.run_example()
+    return test
 
 
 class TestExamples(unittest.TestCase):
@@ -17,7 +27,21 @@ class TestExamples(unittest.TestCase):
             time.sleep(1)
         prog_server.stop()
         raise Exception("Server startup timeout")
-    
+
+    def setUp(self):
+        # set stdout (so it wont print)
+        self._stdout = sys.stdout
+        sys.stdout = StringIO()
+
+    # def test_online_prog(self):
+    #     from examples import online_prog
+
+    #     online_prog.run_example()
+
+    def tearDown(self):
+        # reset stdout
+        sys.stdout = self._stdout
+        
     @classmethod
     def tearDownClass(cls):
         prog_server.stop()
@@ -25,9 +49,15 @@ class TestExamples(unittest.TestCase):
 
 # This allows the module to be executed directly    
 def run_tests():
+    # Create tests for each example
+    for _, name, _ in pkgutil.iter_modules(['examples']):
+        test_func = make_test_function(name)
+        setattr(TestExamples, 'test_{0}'.format(name), test_func)   
+
+    # Run tests
     l = unittest.TestLoader()
     runner = unittest.TextTestRunner()
-    print("\n\nTesting prog_client with prog_server")
+    print("\n\nTesting examples (this may take some time)")
     result = runner.run(l.loadTestsFromTestCase(TestExamples)).wasSuccessful()
 
     if not result:
