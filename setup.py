2f6218a6c5577df5c74b174a9075bfb9f76a8579
Christopher Teubert <christopher.a.teubert@nasa.gov>
2023-05-26 15:48:34 -0700

    3.11 support

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 setup(
     name='prog_server',
     version='1.5.0-pre',
-    description = 'The NASA Prognostics As-A-Service (PaaS) Sandbox (a.k.a. prog_server) is a simplified Software Oriented Architecture (SOA) for performing prognostics of engineering systems. The PaaS Sandbox is a wrapper around the Prognostics Algorithms and Models Packages, allowing 1+ users to access these packages features through a REST API. The package is intended to be used as a research tool to prototype and benchmark Prognostics As-A-Service (PaaS) architectures and work on the challenges facing such architectures',
+    description='The NASA Prognostics As-A-Service (PaaS) Sandbox (a.k.a. prog_server) is a simplified Software Oriented Architecture (SOA) for performing prognostics of engineering systems. The PaaS Sandbox is a wrapper around the Prognostics Algorithms and Models Packages, allowing 1+ users to access these packages features through a REST API. The package is intended to be used as a research tool to prototype and benchmark Prognostics As-A-Service (PaaS) architectures and work on the challenges facing such architectures',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url = 'https://nasa.github.com/progpy/prog_server_guide.html',
-    author = 'Christopher Teubert',
-    author_email = 'christopher.a.teubert@nasa.gov',
-    classifiers = [
+    url='https://nasa.github.com/progpy/prog_server_guide.html',
+    author='Christopher Teubert',
+    author_email='christopher.a.teubert@nasa.gov',
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
         'Intended Audience :: Developers',
@@ -34,17 +34,17 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3 :: Only'
     ],
-    keywords = ['prognostics', 'diagnostics', 'fault detection', 'fdir', 'physics modeling', 'prognostics and health management', 'PHM', 'health management', 'prognostics as a service', 'ivhm'],
-    package_dir = {"":"src"},
-    packages = find_packages(where = 'src'),
-    python_requires='>=3.7, <3.11',
-    install_requires = [
+    keywords=['prognostics', 'diagnostics', 'fault detection', 'fdir', 'physics modeling', 'prognostics and health management', 'PHM', 'health management', 'prognostics as a service', 'ivhm'],
+    package_dir={"":"src"},
+    packages=find_packages(where='src'),
+    python_requires='>=3.7, <3.12',
+    install_requires=[
         'prog_algs',
         'requests',
         'urllib3',
         'flask'
     ],
-    license = 'NOSA',
+    license='NOSA',
     project_urls={  # Optional
         'Bug Reports': 'https://github.com/nasa/prog_server/issues',
         'Docs': 'https://nasa.github.io/progpy/prog_server_guide.html',
