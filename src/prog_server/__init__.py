5a21b835e9026a820393c51e57fc3b8c6197e86b
Christopher Teubert <christopher.a.teubert@nasa.gov>
2023-10-25 15:54:59 -0700

    Update Version #

--- src/prog_server/__init__.py
+++ src/prog_server/__init__.py
@@ -2,7 +2,7 @@
 # National Aeronautics and Space Administration.  All Rights Reserved.
 
 from .models.prog_server import server
-__version__ = '1.6.0-pre'
+__version__ = '1.7.0-pre'
 
 def run(**kwargs):
     """
